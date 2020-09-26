9d863ed65502054dd9764deadf463e979e4bb6bf
prairir <me@ryanprairie.com>
2020-09-26 14:58:06 -0400

    basic folder upload

--- vtcli.py
+++ vtcli.py
@@ -1,27 +1,53 @@
 from pyhocon import ConfigFactory
 import requests
 import pprint
+import os
+import time
+import sys
 
-conf = ConfigFactory.parse_file("./secrets.conf")
 
-headers = {
-    "x-apikey": conf.get("api_key")
-}
+def shutdown(message):
+    print("script is turning off now\nbye :)")
+    sys.exit("Error: {}".format(message))
+
+def printer(dataDict):
+    pp = pprint.PrettyPrinter().pprint(dataDict)
+
+def readResponse(uploadResponse):
+    responses = []
+    for i in uploadResponse:
+        response = requests.get("https://www.virustotal.com/api/v3/analyses/{}".format(i["data"]["id"]), headers=headers)
+        responses.append(response.json())
+
+    return responses
 
 def sendFile(path):
-    files = {'file': open(path)}
+    print(path)
+    uploadFile = open(path)
+    files = {'file': uploadFile}
     response = requests.post("https://www.virustotal.com/api/v3/files", headers=headers, files=files ) 
     return response
 
-def readResponse(uploadResponse):
+def sendFolder(path):
+    responses = []
+    for (dirPath, dirList, fileList) in os.walk(path):
+        if len(fileList) <= 0:
+            shutdown("Directory is empty")
+        printer(fileList)
+        for single in fileList:
+            responses.append(sendFile(single).json())
+            time.sleep(conf["folder_delay"])
+        break
+    return responses
 
-    return requests.get("https://www.virustotal.com/api/v3/analyses/{}".format(uploadResponse["data"]["id"]), headers=headers)
+def main():
+    printer(readResponse(sendFolder(".")))
 
-def printer(dataDict):
-    pp = pprint.PrettyPrinter()
-    pp.pprint(dataDict)
+if __name__ == "__main__":
+    conf = ConfigFactory.parse_file("./secrets.conf")
 
-test = sendFile("./LICENSE")
-printer(test.json())
-printer(readResponse(test.json()).json())
+    headers = {
+        "x-apikey": conf.get("api_key")
+    }
+    main() 
 
