03e0dd22dfa2fd4aaf97bc6f7163bdeb68ac5e5f
prairir <me@ryanprairie.com>
2020-09-26 22:23:01 -0400

    got print options working

--- vtcli.py
+++ vtcli.py
@@ -18,6 +18,19 @@ def shutdown(message):
 # takes any object
 # returns None
 def printer(dataDict):
+    if(arguements.verbose):
+        for out in dataDict:
+            print(out[0])
+            res = out[1].get("data").get("attributes").get("results")
+            for x in res:
+                cat = res[x].get("category")
+                met = res[x].get("method")
+                print("\n\n\tName: {} \n\tCategory: {}\n\tMethod: {}".format(x, cat, met))
+        return
+    if(arguements.raw):
+        pprint.PrettyPrinter().pprint(dataDict)
+        return
+
     for out in dataDict:
         print(out[0])
         res = out[1].get("data").get("attributes").get("stats")
@@ -25,15 +38,6 @@ def printer(dataDict):
         for x in items:
             print("\t" + x[0] + " " +str(x[1]))
         
-
-        #print(out[0])
-        #res = out[1].get("data").get("attributes").get("results")
-        #for x in res:
-         #   cat = res[x].get("category")
-          #  met = res[x].get("method")
-           # print("\n\n\tName: {} \n\tCategory: {}\n\tMethod: {}".format(x, cat, met))
-
-    #pp = pprint.PrettyPrinter().pprint(dataDict)
 
 # reads the response
 # takes a list or response object
@@ -100,14 +104,29 @@ def sendFolder(path):
     return responses
 
 def main():
+    print(arguements.value)
+    print(arguements.url)
     printer(readResponse(sendUrl("https://www.google.ca")))
-
 
     
 if __name__ == "__main__":
     argparser = argparse.ArgumentParser(prog="vtcli", description="CLI tool for virus scanning with Virus Total")
 
     argparser.add_argument("value", nargs=1, metavar="File")
+
+    argparser.add_argument("-u", "--url", action="store_true", help="Scan url")
+
+    argparser.add_argument("-d", "--directory", action="store_true", help="Scan directory")
+
+    argparser.add_argument("-dc", "--directory-continue", action="store_true", help="Scan directory continue on error")
+
+    argparser.add_argument("-c", "--continue", action="store_true", help="Continue on error, only useful with -d")
+
+    argparser.add_argument("-f", "--file", action="store_true", help="Scan file, same as no flags")
+
+    argparser.add_argument("-v", "--verbose", action="store_true", help="More output, better if you want to see specifics")
+
+    argparser.add_argument("-r", "--raw", action="store_true", help="Raw output of json")
 
     arguements = argparser.parse_args()
 
